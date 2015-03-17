78ffccc185b8814f8b4af8175e7def25cdc885b8
Marco Semiao <marco.semiao@gmail.com>
2015-03-17 15:28:05 +0100

    refacto version unique pour java3, java5 et jboss5

---
+++ modules/logger/file/log4jdbc-logger-file-core/src/main/java/fr/ms/log4jdbc/message/AbstractMessage.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of Log4Jdbc.
+ *
+ * Log4Jdbc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Log4Jdbc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Log4Jdbc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package fr.ms.log4jdbc.message;
+
+import java.lang.reflect.Method;
+import java.util.Iterator;
+
+import fr.ms.log4jdbc.utils.Service;
+import fr.ms.log4jdbc.writer.MessageWriter;
+import fr.ms.log4jdbc.writer.MessageWriterFactory;
+import fr.ms.log4jdbc.writer.WrapperMessageWriterFactory;
+
+/**
+ * 
+ * @see <a href="http://marcosemiao4j.wordpress.com">Marco4J</a>
+ * 
+ * 
+ * @author Marco Semiao
+ * 
+ */
+public abstract class AbstractMessage implements MessageProcess {
+
+  private static WrapperMessageWriterFactory factory;
+
+  static {
+    final Iterator providers = Service.providers(WrapperMessageWriterFactory.class);
+
+    while (providers.hasNext()) {
+      try {
+        final WrapperMessageWriterFactory p = (WrapperMessageWriterFactory) providers.next();
+
+        if (p != null && p.isEnabled()) {
+          if (factory == null || factory.getPriority() < p.getPriority()) {
+            factory = p;
+          }
+        }
+      } catch (final Throwable t) {
+        t.printStackTrace();
+      }
+    }
+  }
+
+  public MessageWriter newMessageWriter(final MessageHandler message, final Method method, final Object[] args,
+      final Object invoke, final Throwable exception) {
+    final MessageWriterFactory messageWriterFactory = factory.getMessageWriterFactory();
+    return messageWriterFactory.newMessageWriter(message, method, args, invoke, exception);
+  }
+}
