0e9ceacc069cafc3a0f0565b7526cf0539ad8d69
Marco Semiao <marco.semiao@gmail.com>
2015-06-20 12:30:11 +0200

    correction RdbmsSpecifics DB2 date format

--- modules/rdbms/log4jdbc-rdbms/src/main/java/fr/ms/log4jdbc/rdbms/Db2RdbmsSpecifics.java
+++ modules/rdbms/log4jdbc-rdbms/src/main/java/fr/ms/log4jdbc/rdbms/Db2RdbmsSpecifics.java
@@ -1,5 +1,6 @@
 package fr.ms.log4jdbc.rdbms;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -10,31 +11,32 @@ import java.util.Date;
  */
 public class Db2RdbmsSpecifics implements RdbmsSpecifics {
 
-  private final RdbmsSpecifics genericRdbms = GenericRdbmsSpecifics.getInstance();
+    private final RdbmsSpecifics genericRdbms = GenericRdbmsSpecifics.getInstance();
 
-  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("'TIMESTAMP('''yyyy-MM-dd HH:mm:ss.SSS''')'");
+    private final static String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss.SSS";
 
-  public boolean isRdbms(final String classType) {
-    return classType.equals("com.ibm.db2.jcc.DB2Driver") || classType.equals("COM.ibm.db2.jdbc.app.DB2Driver")
-        || classType.equals("COM.ibm.db2.jdbc.net.DB2Driver");
-  }
-
-  public DataRdbms getData(final Object object) {
-    if (object instanceof Date) {
-      return new GenericDataRdbms(DATE_FORMAT.format((Date) object));
+    public boolean isRdbms(final String classType) {
+	return classType.startsWith("com.ibm.db2") || classType.startsWith("COM.ibm.db2");
     }
-    return genericRdbms.getData(object);
-  }
 
-  public String getTypeQuery(final String sql) {
-    return genericRdbms.getTypeQuery(sql);
-  }
+    public DataRdbms getData(final Object object) {
+	if (object instanceof Date) {
+	    final DateFormat df = new SimpleDateFormat(DATE_PATTERN);
+	    final String dateString = df.format(object);
+	    return new GenericDataRdbms(dateString, "'");
+	}
+	return genericRdbms.getData(object);
+    }
 
-  public String removeComment(String sql) {
-    return genericRdbms.removeComment(sql);
-  }
+    public String getTypeQuery(final String sql) {
+	return genericRdbms.getTypeQuery(sql);
+    }
 
-  public boolean isCaseSensitive() {
-    return genericRdbms.isCaseSensitive();
-  }
+    public String removeComment(final String sql) {
+	return genericRdbms.removeComment(sql);
+    }
+
+    public boolean isCaseSensitive() {
+	return genericRdbms.isCaseSensitive();
+    }
 }
