e4a3d21e38eaea2ac61dd7ba878876f0c7c64ba2
Marco Semiao <marco.semiao@gmail.com>
2015-06-20 12:29:49 +0200

    refacto RdbmsSpecifics

--- modules/rdbms/log4jdbc-rdbms/src/main/java/fr/ms/log4jdbc/rdbms/MySqlRdbmsSpecifics.java
+++ modules/rdbms/log4jdbc-rdbms/src/main/java/fr/ms/log4jdbc/rdbms/MySqlRdbmsSpecifics.java
@@ -1,5 +1,6 @@
 package fr.ms.log4jdbc.rdbms;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 /**
@@ -9,37 +10,49 @@ import java.text.SimpleDateFormat;
  */
 public class MySqlRdbmsSpecifics implements RdbmsSpecifics {
 
-  private final RdbmsSpecifics genericRdbms = GenericRdbmsSpecifics.getInstance();
+    private final RdbmsSpecifics genericRdbms = GenericRdbmsSpecifics.getInstance();
 
-  public boolean isRdbms(final String classType) {
-    return classType.equals("com.mysql.jdbc.Driver");
-  }
+    private final static String TIME_PATTERN = "HH:mm:ss";
 
-  public DataRdbms getData(final Object object) {
-    if (object instanceof java.sql.Time) {
-      return new GenericDataRdbms(new SimpleDateFormat("HH:mm:ss").format(object), "'");
+    private final static String SQL_DATE_PATTERN = "yyyy-MM-dd";
+
+    private final static String UTIL_DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    public boolean isRdbms(final String classType) {
+	return classType.startsWith("com.mysql");
     }
 
-    if (object instanceof java.sql.Date) {
-      return new GenericDataRdbms(new SimpleDateFormat("yyyy-MM-dd").format(object), "'");
+    public DataRdbms getData(final Object object) {
+	if (object instanceof java.sql.Time) {
+	    final DateFormat df = new SimpleDateFormat(TIME_PATTERN);
+	    final String dateString = df.format(object);
+	    return new GenericDataRdbms(dateString, "'");
+	}
+
+	if (object instanceof java.sql.Date) {
+	    final DateFormat df = new SimpleDateFormat(SQL_DATE_PATTERN);
+	    final String dateString = df.format(object);
+	    return new GenericDataRdbms(dateString, "'");
+	}
+
+	if (object instanceof java.util.Date) { // (includes java.sql.Timestamp)
+	    final DateFormat df = new SimpleDateFormat(UTIL_DATE_PATTERN);
+	    final String dateString = df.format(object);
+	    return new GenericDataRdbms(dateString, "'");
+	}
+
+	return genericRdbms.getData(object);
     }
 
-    if (object instanceof java.util.Date) { // (includes java.sql.Timestamp)
-      return new GenericDataRdbms(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(object), "'");
+    public String getTypeQuery(final String sql) {
+	return genericRdbms.getTypeQuery(sql);
     }
 
-    return genericRdbms.getData(object);
-  }
+    public String removeComment(final String sql) {
+	return genericRdbms.removeComment(sql);
+    }
 
-  public String getTypeQuery(final String sql) {
-    return genericRdbms.getTypeQuery(sql);
-  }
-
-  public String removeComment(String sql) {
-    return genericRdbms.removeComment(sql);
-  }
-
-  public boolean isCaseSensitive() {
-    return genericRdbms.isCaseSensitive();
-  }
+    public boolean isCaseSensitive() {
+	return genericRdbms.isCaseSensitive();
+    }
 }
